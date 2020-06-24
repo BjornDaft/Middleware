42ab83cd11e3539c9ab22f3a9982a0ecd64df9f5
Chococraco <etienne.jouvencon@wanadoo.fr>
2020-06-24 16:36:48 +0200

    BDD Connection (don't work)

--- lifeleft/src/main/java/com/lifeleft/CheckFileService.java
+++ lifeleft/src/main/java/com/lifeleft/CheckFileService.java
@@ -8,6 +8,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.sql.*;
+
 
 @WebService(serviceName = "CheckFile")
 public class CheckFileService {
@@ -41,6 +43,42 @@ public class CheckFileService {
 
         //If the percent of French word is lower than we want
         return "Error";
+    }
+
+    @WebMethod
+    public String testBDD(String word){
+
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return "test";
+
+        /*try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            //try {
+                /*Connection con = DriverManager.getConnection("jdbc:oracle:thin@localhost:1521:ORCLCDB.localdomain","choco","choco");
+                Statement statement = con.createStatement();*/
+                /*String sql = "select * from dico where words = " + word + ";";
+                ResultSet rs = statement.executeQuery(sql);*/
+                /*while(rs.next()){
+                    return "On a";
+                }*/
+                //return "On a pa";
+
+            //} catch (SQLException throwables) {
+                //throwables.printStackTrace();
+               // return "crash";
+            //}
+
+
+        //} catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+            //return "crash";
+        //}
+
     }
 
     //Get percent of French word in a text
